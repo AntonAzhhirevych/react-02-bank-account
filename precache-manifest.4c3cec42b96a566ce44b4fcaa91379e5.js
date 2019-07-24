688851da308888250892e3664e9796ff2a40424a
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-24 16:17:53 +0300

    Updates

---
+++ precache-manifest.4c3cec42b96a566ce44b4fcaa91379e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92d44210aa412c776bae2024e25ce42",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "c798b5283b3aded668c7",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "1096d265531aa55fa77c",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.ffcca1cc.chunk.css"
+  },
+  {
+    "revision": "c798b5283b3aded668c7",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.f744dc37.chunk.js"
+  },
+  {
+    "revision": "1096d265531aa55fa77c",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.e5612f28.chunk.js"
+  },
+  {
+    "revision": "671ff8e999e03896268d",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.0f0e45e8.js"
+  }
+]);
